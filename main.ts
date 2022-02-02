d0c6bb70871f3d234e9b8d66ccbe5100f82ccd82
yasujisasaki60 <98871840+yasujisasaki60@users.noreply.github.com>
2022-02-02 16:22:19 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("Hello!everyone")
+    basic.showString("My name is Jason ")
+    basic.showString("Thank you")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+})
